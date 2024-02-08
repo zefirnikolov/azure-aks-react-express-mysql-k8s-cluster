08d24cfb072d48c9b224a65fd948e2d6d230d962
Zefir Nikolov <113708651+zefirnikolov@users.noreply.github.com>
2024-02-08 20:59:45 +0200

    Update WhyChooseUs.js

--- client/src/components/HomePageComponents/WhyChooseUs.js
+++ client/src/components/HomePageComponents/WhyChooseUs.js
@@ -6,7 +6,7 @@ const WhyChooseUs = () => {
     <div className="why-choose-us">
       <img src="https://cdn.pixabay.com/photo/2016/04/23/22/35/healthy-food-1348430_1280.jpg" alt="Why choose us" />
       <div className="why-choose-us-content">
-        <h2>Why Choose Us?</h2>
+        <h2>Choose US!</h2>
         <p>We stand out from the competition due to our commitment to exceptional quality, personalized service, and unbeatable value for your money. Our selection of foods and other goods are carefully sourced to ensure maximum freshness and quality. Trust us for all your shopping needs.</p>
       </div>
     </div>
