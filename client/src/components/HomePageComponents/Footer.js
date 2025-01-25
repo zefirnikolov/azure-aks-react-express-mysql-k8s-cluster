ecba7386687a43c318e7a98b5010cd676910b523
Zefir Nikolov <113708651+zefirnikolov@users.noreply.github.com>
2025-01-25 14:51:44 -0500

    upgrade footer

--- client/src/components/HomePageComponents/Footer.js
+++ client/src/components/HomePageComponents/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="footer">
-      <p>© 2023 K8s microservice architecture deployed on Azure AKS. All Rights Reserved.</p>
+      <p>© 2025 K8s microservice architecture deployed on Azure AKS. All Rights Reserved.</p>
       <p>
         <a href="/">Privacy Policy</a> | 
         <a href="/">Terms of Service</a>
